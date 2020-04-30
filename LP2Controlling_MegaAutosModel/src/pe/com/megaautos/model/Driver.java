1d50ac9707088b9b74450a2a001c2d2cb66e4450
Rodrigo <Rodrigo@DESKTOP-TFIE6KL>
2020-04-30 00:12:13 -0500

    Se movieron todas las clases al paquete pe.com.megaautos.model

--- LP2Controlling_MegaAutosModel/src/pe/com/megaautos/model/Driver.java
+++ LP2Controlling_MegaAutosModel/src/pe/com/megaautos/model/Driver.java
@@ -6,6 +6,18 @@ package pe.com.megaautos.model;
  * @author nicolas
  */
 public class Driver {
+    private String nombre;
+    
     public Driver(){
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
 }

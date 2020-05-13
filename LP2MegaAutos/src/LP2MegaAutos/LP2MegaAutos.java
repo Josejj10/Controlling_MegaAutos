6f84d3802a8a1d6d6c9a0ee78789f74fe3e27a62
Jose Javier <jose.javier@pucp.edu.pe>
2020-05-13 02:04:21 -0500

    Merge origin/master
    Conflicts:
    	.gitignore

--- LP2MegaAutos/src/LP2MegaAutos/LP2MegaAutos.java
+++ LP2MegaAutos/src/LP2MegaAutos/LP2MegaAutos.java
@@ -5,6 +5,16 @@
  */
 package LP2MegaAutos;
 
+import java.util.ArrayList;
+import pe.com.megaautos.dao.VehiculoDAO;
+import pe.com.megaautos.model.Vehiculo;
+import pe.com.megaautos.mysql.VehiculoMySQL;
+import pe.com.megaautos.mysql.ClienteMySQL;
+import pe.com.megaautos.dao.ClienteDAO;
+import pe.com.megaautos.model.Cliente;
+import pe.com.megaautos.dao.DriverDAO;
+import pe.com.megaautos.model.Driver;
+import pe.com.megaautos.mysql.DriverMySQL;
 /**
  *
  * @author Jose
@@ -16,6 +26,27 @@ public class LP2MegaAutos {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //Vehiculo vehiculo= new Vehiculo("");
+        /*ArrayList<Vehiculo> vehiculos =
+                new ArrayList<>();
+        
+        VehiculoDAO daoVehiculo = 
+                new VehiculoMySQL();
+        
+        daoVehiculo.insertar(vehiculo);
+        vehiculos = daoVehiculo.listar();
+        
+        */
+        Driver dr = new Driver(1/10);
+        Driver dr2 = new Driver(2/5);
+        DriverDAO daoDriver = new DriverMySQL();
+        daoDriver.insertar(dr);
+        daoDriver.insertar(dr2);
+        ArrayList<Driver> drivers = new ArrayList<>();
+        drivers = daoDriver.listar();
+        for(Driver d : drivers){
+            System.out.println(d.getFormula());
+        }
     }
 
 }

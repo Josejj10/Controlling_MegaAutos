7c8b3ede3007b0e0641dd4d1f39459bf85637e8f
Jose Javier <jose.javier@pucp.edu.pe>
2020-05-13 01:14:00 -0500

    Merge origin/master
    Conflicts:
    	.gitignore

--- LP2MegaAutosController/src/pe/com/megaautos/mysql/ClienteMySQL.java
+++ LP2MegaAutosController/src/pe/com/megaautos/mysql/ClienteMySQL.java
@@ -8,6 +8,7 @@ package pe.com.megaautos.mysql;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import pe.com.megaautos.config.DBManager;
 import pe.com.megaautos.dao.ClienteDAO;
@@ -22,8 +23,35 @@ public class ClienteMySQL implements ClienteDAO {
 
     @Override
     public Cliente buscar(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Cliente cliente = new Cliente();
+        try{
+            //Registrar el JAR de conexión
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            //Establecer una conexión a la BD
+            Connection con = DriverManager.
+            getConnection(DBManager.url,DBManager.user, DBManager.password);
+            // Llama a un select * from cliente where ID_CLIENTE = id
+            CallableStatement cs = con.prepareCall(
+                    "{call BUSCAR_CLIENTE(?)}");
+            cs.setInt("_ID", id);
+            ResultSet rs = cs.executeQuery();
+            //Recorrer todas las filas que devuelve la ejecucion sentencia
+            while(rs.next()){
+                cliente.setId(rs.getInt("ID_CLIENTE"));
+                cliente.setNombre(rs.getString("NOMBRE"));
+                cliente.setTipoCliente(rs.getString("TIPO_CLIENTE"));
+                cliente.setTipoDocumento(rs.getString("TIPO_DOC"));
+                cliente.setCorreo(rs.getString("CORREO"));
+                cliente.setTelefono(rs.getString("TELEFONO"));
+            }
+            //cerrar conexion
+            con.close();
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        return cliente;
     }
+    
 
     @Override
     public int insertar(Cliente cliente) {
@@ -41,14 +69,18 @@ public class ClienteMySQL implements ClienteDAO {
             // En el procedure de MySQL, cambiara el nombre del tipo vehiculo
             // Por su id, para poder insertarlo en la tabla 
             cs.registerOutParameter("_ID_CLIENTE", java.sql.Types.INTEGER);
+            // TODO AL NOMBRE AGREGARLE SACAPALABRAS QUE BORRE LOS DOBLE ESPACIOS
             cs.setString("_NOMBRE", cliente.getNombre().toUpperCase());
-            cs.setString("_TIPO_CLIENTE", vehiculo.getTipoVehiculo().toUpperCase());
-            cs.setInt(2, vehiculo.getPropietario().getId());
+            cs.setString("_TIPO_CLIENTE", cliente.getTipoCliente().toUpperCase());
+            cs.setString("_TIPO_DOCUMENTO", cliente.getTipoDocumento().toUpperCase());
+            cs.setString("_NUMERO_DOCUMENTO", cliente.getNumDocumento());
+            cs.setString("_CORREO", cliente.getCorreo());
+            cs.setString("_TELEFONO",cliente.getTelefono());
             cs.executeUpdate();
-            rpta = cs.getInt("_ID_VEHICULO");
+            rpta = cs.getInt("_ID_CLIENTE");
             con.close();
             // Actualiza el ID del vehiculo insertado para tenerlo en Java
-            vehiculo.setId(rpta);
+            cliente.setId(rpta);
         }catch(Exception ex){
              System.out.println(ex.getMessage());
         }
@@ -67,7 +99,37 @@ public class ClienteMySQL implements ClienteDAO {
 
     @Override
     public ArrayList<Cliente> listar() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ArrayList<Cliente> clientes = new ArrayList<>();
+        try{
+            //Registrar el JAR de conexión
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            //Establecer una conexión a la BD
+            Connection con = DriverManager.
+            getConnection(DBManager.url,DBManager.user, DBManager.password);
+            // Listar cliente devuelve una lista de clientes
+            // con ID_Cliente, nombre, tipo_cliente, tipo_doc
+            // numero_doc, correo y telefono
+            CallableStatement cs = con.prepareCall(
+                    "{call LISTAR_CLIENTE()}");
+            ResultSet rs = cs.executeQuery();
+            //Recorrer todas las filas que devuelve la ejecucion sentencia
+            while(rs.next()){
+                Cliente cliente = new Cliente();
+                cliente.setId(rs.getInt("ID_CLIENTE"));
+                cliente.setNombre(rs.getString("NOMBRE"));
+                cliente.setTipoCliente(rs.getString("TIPO_CLIENTE"));
+                cliente.setTipoDocumento(rs.getString("TIPO_DOC"));
+                cliente.setCorreo(rs.getString("CORREO"));
+                cliente.setTelefono(rs.getString("TELEFONO"));
+                clientes.add(cliente);
+            }
+            //cerrar conexion
+            con.close();
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        //Devolviendo los vehiculos
+        return clientes;    
     }
     
 }

ae070b27fe6a159742db002b5c956dfd76a24b08
Jose Javier <jose.javier@pucp.edu.pe>
2020-05-12 21:38:57 -0500

    Cambiado Local por Sede

---
+++ LP2MegaAutosController/src/pe/com/megaautos/config/DBManager.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.com.megaautos.config;
+
+/**
+ *
+ * @author UsuarioA
+ */
+public class DBManager {
+    public static String url = "jdbc:mysql://databaseta.ct02htypf4pb.us-east-1.rds.amazonaws.com:3306/megaautosDB";
+    public static String user = "admin";
+    public static String password = "megaautos201";
+}

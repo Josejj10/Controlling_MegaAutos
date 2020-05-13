f5b6da29aff53b32ec5401deacb72c4e6f54dd67
Jose Javier <jose.javier@pucp.edu.pe>
2020-05-12 20:44:51 -0500

    Arreglado LP2MegaAutos

---
+++ LP2MegaAutos/src/LP2MegaAutos/LP2MegaAutos.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LP2MegaAutos;
+
+/**
+ *
+ * @author Jose
+ */
+public class LP2MegaAutos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}

be4d7050c3914491bb00cf21761cd05526533589
Jose Shimabukuro <seizenshima@yahoo.es>
2020-05-26 18:47:53 -0500

    f

--- ProyectosNET/LP2MegaAutos/pantallaActualizarUsuarios.Designer.cs
+++ ProyectosNET/LP2MegaAutos/pantallaActualizarUsuarios.Designer.cs
@@ -28,8 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.systemColorsExtender1 = new LP2MegaAutos.SystemColorsExtender();
-            this.roundedPanelExtender1 = new LP2MegaAutos.RoundedPanelExtender();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.btn_AZ = new System.Windows.Forms.Button();
             this.btn_ZA = new System.Windows.Forms.Button();
@@ -38,15 +36,14 @@
             this.rpn_ListaUsuarios = new LP2MegaAutos.RoundedPanel();
             this.lbl_ListaUsuarios = new System.Windows.Forms.Label();
             this.btn_AgregarUsuario = new System.Windows.Forms.Button();
+            this.btn_EditarUsuario = new System.Windows.Forms.Button();
             this.rpn_ListaUsuarios.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.systemColorsExtender1.SetBackColorSystem(this.textBox1, LP2MegaAutos.ColoresSistema.FrontBackground);
             this.textBox1.Font = new System.Drawing.Font("Lato", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.systemColorsExtender1.SetForeColorSystem(this.textBox1, LP2MegaAutos.ColoresSistema.HighContrast);
-            this.textBox1.Location = new System.Drawing.Point(40, 16);
+            this.textBox1.Location = new System.Drawing.Point(36, 18);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(148, 30);
             this.textBox1.TabIndex = 0;
@@ -54,9 +51,7 @@
             // btn_AZ
             // 
             this.btn_AZ.BackColor = System.Drawing.Color.Transparent;
-            this.systemColorsExtender1.SetBackColorSystem(this.btn_AZ, LP2MegaAutos.ColoresSistema.Transparent);
             this.btn_AZ.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.systemColorsExtender1.SetForeColorSystem(this.btn_AZ, LP2MegaAutos.ColoresSistema.HighContrast);
             this.btn_AZ.Location = new System.Drawing.Point(36, 65);
             this.btn_AZ.Name = "btn_AZ";
             this.btn_AZ.Size = new System.Drawing.Size(130, 35);
@@ -67,10 +62,8 @@
             // btn_ZA
             // 
             this.btn_ZA.BackColor = System.Drawing.Color.Transparent;
-            this.systemColorsExtender1.SetBackColorSystem(this.btn_ZA, LP2MegaAutos.ColoresSistema.Transparent);
             this.btn_ZA.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_ZA.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
-            this.systemColorsExtender1.SetForeColorSystem(this.btn_ZA, LP2MegaAutos.ColoresSistema.HighContrast);
             this.btn_ZA.Location = new System.Drawing.Point(172, 65);
             this.btn_ZA.Name = "btn_ZA";
             this.btn_ZA.Size = new System.Drawing.Size(130, 35);
@@ -81,10 +74,8 @@
             // btn_Antiguo
             // 
             this.btn_Antiguo.BackColor = System.Drawing.Color.Transparent;
-            this.systemColorsExtender1.SetBackColorSystem(this.btn_Antiguo, LP2MegaAutos.ColoresSistema.Transparent);
             this.btn_Antiguo.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_Antiguo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
-            this.systemColorsExtender1.SetForeColorSystem(this.btn_Antiguo, LP2MegaAutos.ColoresSistema.HighContrast);
             this.btn_Antiguo.Location = new System.Drawing.Point(308, 65);
             this.btn_Antiguo.Name = "btn_Antiguo";
             this.btn_Antiguo.Size = new System.Drawing.Size(130, 35);
@@ -95,10 +86,8 @@
             // btn_Reciente
             // 
             this.btn_Reciente.BackColor = System.Drawing.Color.Transparent;
-            this.systemColorsExtender1.SetBackColorSystem(this.btn_Reciente, LP2MegaAutos.ColoresSistema.Transparent);
             this.btn_Reciente.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_Reciente.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
-            this.systemColorsExtender1.SetForeColorSystem(this.btn_Reciente, LP2MegaAutos.ColoresSistema.HighContrast);
             this.btn_Reciente.Location = new System.Drawing.Point(444, 65);
             this.btn_Reciente.Name = "btn_Reciente";
             this.btn_Reciente.Size = new System.Drawing.Size(130, 35);
@@ -108,18 +97,11 @@
             // 
             // rpn_ListaUsuarios
             // 
-            this.roundedPanelExtender1.SetArcoRBorde(this.rpn_ListaUsuarios, 15);
-            this.roundedPanelExtender1.SetArcoRPanel(this.rpn_ListaUsuarios, 15);
             this.rpn_ListaUsuarios.ArcRadiusBorde = 15;
             this.rpn_ListaUsuarios.ArcRadiusPanel = 15;
-            this.systemColorsExtender1.SetBackColorSystem(this.rpn_ListaUsuarios, LP2MegaAutos.ColoresSistema.FrontBackground);
-            this.roundedPanelExtender1.SetColorSistemaBorde(this.rpn_ListaUsuarios, LP2MegaAutos.ColoresSistema.BackBackground);
-            this.roundedPanelExtender1.SetColorSistemaPanel(this.rpn_ListaUsuarios, LP2MegaAutos.ColoresSistema.Morado);
             this.rpn_ListaUsuarios.Controls.Add(this.btn_AgregarUsuario);
             this.rpn_ListaUsuarios.Controls.Add(this.lbl_ListaUsuarios);
-            this.roundedPanelExtender1.SetCorners(this.rpn_ListaUsuarios, Plasmoid.Extensions.RectangleEdgeFilter.None);
             this.rpn_ListaUsuarios.CornersRound = Plasmoid.Extensions.RectangleEdgeFilter.None;
-            this.systemColorsExtender1.SetForeColorSystem(this.rpn_ListaUsuarios, LP2MegaAutos.ColoresSistema.HighContrast);
             this.rpn_ListaUsuarios.Location = new System.Drawing.Point(36, 106);
             this.rpn_ListaUsuarios.MenosHeightBorde = 1;
             this.rpn_ListaUsuarios.MenosHeightPanel = 1;
@@ -138,11 +120,9 @@
             // 
             this.lbl_ListaUsuarios.AutoSize = true;
             this.lbl_ListaUsuarios.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(174)))), ((int)(((byte)(32)))), ((int)(((byte)(226)))));
-            this.systemColorsExtender1.SetBackColorSystem(this.lbl_ListaUsuarios, LP2MegaAutos.ColoresSistema.Morado);
             this.lbl_ListaUsuarios.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.lbl_ListaUsuarios.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_ListaUsuarios.ForeColor = System.Drawing.Color.White;
-            this.systemColorsExtender1.SetForeColorSystem(this.lbl_ListaUsuarios, LP2MegaAutos.ColoresSistema.HighContrast);
             this.lbl_ListaUsuarios.Location = new System.Drawing.Point(215, 1);
             this.lbl_ListaUsuarios.Name = "lbl_ListaUsuarios";
             this.lbl_ListaUsuarios.Size = new System.Drawing.Size(108, 29);
@@ -153,12 +133,10 @@
             // btn_AgregarUsuario
             // 
             this.btn_AgregarUsuario.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(65)))), ((int)(((byte)(33)))));
-            this.systemColorsExtender1.SetBackColorSystem(this.btn_AgregarUsuario, LP2MegaAutos.ColoresSistema.PrincipalRojo);
             this.btn_AgregarUsuario.FlatAppearance.BorderSize = 0;
             this.btn_AgregarUsuario.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_AgregarUsuario.Font = new System.Drawing.Font("Montserrat", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_AgregarUsuario.ForeColor = System.Drawing.Color.White;
-            this.systemColorsExtender1.SetForeColorSystem(this.btn_AgregarUsuario, LP2MegaAutos.ColoresSistema.HighContrast);
             this.btn_AgregarUsuario.Location = new System.Drawing.Point(432, 4);
             this.btn_AgregarUsuario.Name = "btn_AgregarUsuario";
             this.btn_AgregarUsuario.Size = new System.Drawing.Size(94, 26);
@@ -167,11 +145,20 @@
             this.btn_AgregarUsuario.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             this.btn_AgregarUsuario.UseVisualStyleBackColor = false;
             // 
+            // btn_EditarUsuario
+            // 
+            this.btn_EditarUsuario.Location = new System.Drawing.Point(487, 162);
+            this.btn_EditarUsuario.Name = "btn_EditarUsuario";
+            this.btn_EditarUsuario.Size = new System.Drawing.Size(75, 23);
+            this.btn_EditarUsuario.TabIndex = 6;
+            this.btn_EditarUsuario.Text = "Editar";
+            this.btn_EditarUsuario.UseVisualStyleBackColor = true;
+            // 
             // pantallaActualizarUsuarios
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.systemColorsExtender1.SetBackColorSystem(this, LP2MegaAutos.ColoresSistema.FrontBackground);
+            this.Controls.Add(this.btn_EditarUsuario);
             this.Controls.Add(this.rpn_ListaUsuarios);
             this.Controls.Add(this.btn_Reciente);
             this.Controls.Add(this.btn_Antiguo);
@@ -179,7 +166,6 @@
             this.Controls.Add(this.btn_AZ);
             this.Controls.Add(this.textBox1);
             this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
-            this.systemColorsExtender1.SetForeColorSystem(this, LP2MegaAutos.ColoresSistema.HighContrast);
             this.Name = "pantallaActualizarUsuarios";
             this.Size = new System.Drawing.Size(611, 470);
             this.rpn_ListaUsuarios.ResumeLayout(false);
@@ -190,9 +176,6 @@
         }
 
         #endregion
-
-        private SystemColorsExtender systemColorsExtender1;
-        private RoundedPanelExtender roundedPanelExtender1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button btn_AZ;
         private System.Windows.Forms.Button btn_ZA;
@@ -201,5 +184,6 @@
         private RoundedPanel rpn_ListaUsuarios;
         private System.Windows.Forms.Button btn_AgregarUsuario;
         private System.Windows.Forms.Label lbl_ListaUsuarios;
+        private System.Windows.Forms.Button btn_EditarUsuario;
     }
 }

7f92a0d05484f21f9089bd42a6916e4265b415f7
Nicolas Cossio <nicolas.cossio@pucp.edu.pe>
2020-05-27 19:46:19 -0500

    Pantallas de area de trabajo completas

---
+++ ProyectosNET/LP2MegaAutos/pantallaAgregarAreaTrabajo.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace LP2MegaAutos
+{
+    partial class pantallaAgregarAreaTrabajo
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rpTopMain = new LP2MegaAutos.RoundedPanel();
+            this.txt_AgregarAreaTrabajo = new System.Windows.Forms.TextBox();
+            this.roundedPanel1 = new LP2MegaAutos.RoundedPanel();
+            this.btn_guardar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnl_nombAreaTrab = new System.Windows.Forms.Panel();
+            this.rnd_color_1 = new LP2MegaAutos.RoundedPanel();
+            this.txt_nombAreaTrab = new System.Windows.Forms.TextBox();
+            this.lbl_nombAreaTrab = new System.Windows.Forms.Label();
+            this.tagsExtender1 = new LP2MegaAutos.TagsExtender();
+            this.roundedPanel1.SuspendLayout();
+            this.pnl_nombAreaTrab.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rpTopMain
+            // 
+            this.rpTopMain.ArcRadiusBorde = 15;
+            this.rpTopMain.ArcRadiusPanel = 15;
+            this.rpTopMain.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(245)))), ((int)(((byte)(246)))));
+            this.rpTopMain.ColorBorde = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(245)))), ((int)(((byte)(246)))));
+            this.rpTopMain.ColorPanel = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(140)))), ((int)(((byte)(161)))));
+            this.rpTopMain.CornersRound = Plasmoid.Extensions.RectangleEdgeFilter.Top;
+            this.rpTopMain.Location = new System.Drawing.Point(0, 3);
+            this.rpTopMain.MenosHeightBorde = 1;
+            this.rpTopMain.MenosHeightPanel = 1;
+            this.rpTopMain.MenosWidthBorde = 1;
+            this.rpTopMain.MenosWidthPanel = 1;
+            this.rpTopMain.Name = "rpTopMain";
+            this.rpTopMain.PorcLuzColorBorde = 0;
+            this.rpTopMain.Size = new System.Drawing.Size(611, 40);
+            this.rpTopMain.TabIndex = 31;
+            this.tagsExtender1.SetTag2(this.rpTopMain, null);
+            this.tagsExtender1.SetTag3(this.rpTopMain, null);
+            this.tagsExtender1.SetTagFontName(this.rpTopMain, fontsD.Ninguno);
+            this.tagsExtender1.SetTagFontSize(this.rpTopMain, 10F);
+            this.tagsExtender1.SetTagFontStyle(this.rpTopMain, System.Drawing.FontStyle.Regular);
+            this.rpTopMain.XBorde = 0;
+            this.rpTopMain.XPanel = 0;
+            this.rpTopMain.YBorde = 0;
+            this.rpTopMain.YPanel = 0;
+            // 
+            // txt_AgregarAreaTrabajo
+            // 
+            this.txt_AgregarAreaTrabajo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_AgregarAreaTrabajo.Font = new System.Drawing.Font("Lato", 21.75F, System.Drawing.FontStyle.Bold);
+            this.txt_AgregarAreaTrabajo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(40)))), ((int)(((byte)(42)))));
+            this.txt_AgregarAreaTrabajo.Location = new System.Drawing.Point(26, 44);
+            this.txt_AgregarAreaTrabajo.Name = "txt_AgregarAreaTrabajo";
+            this.txt_AgregarAreaTrabajo.Size = new System.Drawing.Size(441, 35);
+            this.txt_AgregarAreaTrabajo.TabIndex = 43;
+            this.tagsExtender1.SetTag2(this.txt_AgregarAreaTrabajo, null);
+            this.tagsExtender1.SetTag3(this.txt_AgregarAreaTrabajo, null);
+            this.tagsExtender1.SetTagFontName(this.txt_AgregarAreaTrabajo, fontsD.Lato);
+            this.tagsExtender1.SetTagFontSize(this.txt_AgregarAreaTrabajo, 22F);
+            this.tagsExtender1.SetTagFontStyle(this.txt_AgregarAreaTrabajo, System.Drawing.FontStyle.Bold);
+            this.txt_AgregarAreaTrabajo.Text = "Agregar nueva Área de Trabajo";
+            // 
+            // roundedPanel1
+            // 
+            this.roundedPanel1.ArcRadiusBorde = 5;
+            this.roundedPanel1.ArcRadiusPanel = 5;
+            this.roundedPanel1.ColorBorde = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(199)))), ((int)(((byte)(45)))));
+            this.roundedPanel1.ColorPanel = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(199)))), ((int)(((byte)(45)))));
+            this.roundedPanel1.Controls.Add(this.btn_guardar);
+            this.roundedPanel1.CornersRound = Plasmoid.Extensions.RectangleEdgeFilter.All;
+            this.roundedPanel1.Location = new System.Drawing.Point(165, 184);
+            this.roundedPanel1.MenosHeightBorde = 1;
+            this.roundedPanel1.MenosHeightPanel = 1;
+            this.roundedPanel1.MenosWidthBorde = 1;
+            this.roundedPanel1.MenosWidthPanel = 1;
+            this.roundedPanel1.Name = "roundedPanel1";
+            this.roundedPanel1.PorcLuzColorBorde = 0;
+            this.roundedPanel1.Size = new System.Drawing.Size(280, 35);
+            this.roundedPanel1.TabIndex = 42;
+            this.tagsExtender1.SetTag2(this.roundedPanel1, null);
+            this.tagsExtender1.SetTag3(this.roundedPanel1, null);
+            this.tagsExtender1.SetTagFontName(this.roundedPanel1, fontsD.Ninguno);
+            this.tagsExtender1.SetTagFontSize(this.roundedPanel1, 10F);
+            this.tagsExtender1.SetTagFontStyle(this.roundedPanel1, System.Drawing.FontStyle.Regular);
+            this.roundedPanel1.XBorde = 0;
+            this.roundedPanel1.XPanel = 0;
+            this.roundedPanel1.YBorde = 0;
+            this.roundedPanel1.YPanel = 0;
+            // 
+            // btn_guardar
+            // 
+            this.btn_guardar.BackColor = System.Drawing.Color.Transparent;
+            this.btn_guardar.FlatAppearance.BorderSize = 0;
+            this.btn_guardar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btn_guardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_guardar.Font = new System.Drawing.Font("Lato", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_guardar.ForeColor = System.Drawing.Color.White;
+            this.btn_guardar.Location = new System.Drawing.Point(1, 1);
+            this.btn_guardar.Name = "btn_guardar";
+            this.btn_guardar.Size = new System.Drawing.Size(278, 33);
+            this.btn_guardar.TabIndex = 0;
+            this.tagsExtender1.SetTag2(this.btn_guardar, null);
+            this.tagsExtender1.SetTag3(this.btn_guardar, null);
+            this.tagsExtender1.SetTagFontName(this.btn_guardar, fontsD.Lato);
+            this.tagsExtender1.SetTagFontSize(this.btn_guardar, 15F);
+            this.tagsExtender1.SetTagFontStyle(this.btn_guardar, System.Drawing.FontStyle.Bold);
+            this.btn_guardar.Text = "Guardar cambios";
+            this.btn_guardar.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(112)))), ((int)(((byte)(112)))));
+            this.panel1.Location = new System.Drawing.Point(11, 89);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(589, 2);
+            this.panel1.TabIndex = 39;
+            this.tagsExtender1.SetTag2(this.panel1, null);
+            this.tagsExtender1.SetTag3(this.panel1, null);
+            this.tagsExtender1.SetTagFontName(this.panel1, fontsD.Ninguno);
+            this.tagsExtender1.SetTagFontSize(this.panel1, 10F);
+            this.tagsExtender1.SetTagFontStyle(this.panel1, System.Drawing.FontStyle.Regular);
+            // 
+            // pnl_nombAreaTrab
+            // 
+            this.pnl_nombAreaTrab.Controls.Add(this.rnd_color_1);
+            this.pnl_nombAreaTrab.Controls.Add(this.txt_nombAreaTrab);
+            this.pnl_nombAreaTrab.Controls.Add(this.lbl_nombAreaTrab);
+            this.pnl_nombAreaTrab.Location = new System.Drawing.Point(160, 111);
+            this.pnl_nombAreaTrab.Margin = new System.Windows.Forms.Padding(4);
+            this.pnl_nombAreaTrab.Name = "pnl_nombAreaTrab";
+            this.pnl_nombAreaTrab.Size = new System.Drawing.Size(292, 41);
+            this.pnl_nombAreaTrab.TabIndex = 38;
+            this.tagsExtender1.SetTag2(this.pnl_nombAreaTrab, null);
+            this.tagsExtender1.SetTag3(this.pnl_nombAreaTrab, null);
+            this.tagsExtender1.SetTagFontName(this.pnl_nombAreaTrab, fontsD.Ninguno);
+            this.tagsExtender1.SetTagFontSize(this.pnl_nombAreaTrab, 10F);
+            this.tagsExtender1.SetTagFontStyle(this.pnl_nombAreaTrab, System.Drawing.FontStyle.Regular);
+            // 
+            // rnd_color_1
+            // 
+            this.rnd_color_1.ArcRadiusBorde = 15;
+            this.rnd_color_1.ArcRadiusPanel = 15;
+            this.rnd_color_1.ColorBorde = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(65)))), ((int)(((byte)(94)))));
+            this.rnd_color_1.ColorPanel = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(65)))), ((int)(((byte)(94)))));
+            this.rnd_color_1.CornersRound = Plasmoid.Extensions.RectangleEdgeFilter.All;
+            this.rnd_color_1.Location = new System.Drawing.Point(2, 4);
+            this.rnd_color_1.MenosHeightBorde = 1;
+            this.rnd_color_1.MenosHeightPanel = 1;
+            this.rnd_color_1.MenosWidthBorde = 1;
+            this.rnd_color_1.MenosWidthPanel = 1;
+            this.rnd_color_1.Name = "rnd_color_1";
+            this.rnd_color_1.PorcLuzColorBorde = 0;
+            this.rnd_color_1.Size = new System.Drawing.Size(3, 36);
+            this.rnd_color_1.TabIndex = 2;
+            this.tagsExtender1.SetTag2(this.rnd_color_1, null);
+            this.tagsExtender1.SetTag3(this.rnd_color_1, null);
+            this.tagsExtender1.SetTagFontName(this.rnd_color_1, fontsD.Ninguno);
+            this.tagsExtender1.SetTagFontSize(this.rnd_color_1, 10F);
+            this.tagsExtender1.SetTagFontStyle(this.rnd_color_1, System.Drawing.FontStyle.Regular);
+            this.rnd_color_1.XBorde = 0;
+            this.rnd_color_1.XPanel = 0;
+            this.rnd_color_1.YBorde = 0;
+            this.rnd_color_1.YPanel = 0;
+            // 
+            // txt_nombAreaTrab
+            // 
+            this.txt_nombAreaTrab.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(244)))), ((int)(((byte)(245)))));
+            this.txt_nombAreaTrab.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_nombAreaTrab.Font = new System.Drawing.Font("Lato", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_nombAreaTrab.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
+            this.txt_nombAreaTrab.Location = new System.Drawing.Point(7, 18);
+            this.txt_nombAreaTrab.Name = "txt_nombAreaTrab";
+            this.txt_nombAreaTrab.Size = new System.Drawing.Size(284, 23);
+            this.txt_nombAreaTrab.TabIndex = 2;
+            this.tagsExtender1.SetTag2(this.txt_nombAreaTrab, null);
+            this.tagsExtender1.SetTag3(this.txt_nombAreaTrab, null);
+            this.tagsExtender1.SetTagFontName(this.txt_nombAreaTrab, fontsD.Lato);
+            this.tagsExtender1.SetTagFontSize(this.txt_nombAreaTrab, 14F);
+            this.tagsExtender1.SetTagFontStyle(this.txt_nombAreaTrab, System.Drawing.FontStyle.Bold);
+            // 
+            // lbl_nombAreaTrab
+            // 
+            this.lbl_nombAreaTrab.AutoSize = true;
+            this.lbl_nombAreaTrab.Font = new System.Drawing.Font("Lato", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_nombAreaTrab.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
+            this.lbl_nombAreaTrab.Location = new System.Drawing.Point(4, 2);
+            this.lbl_nombAreaTrab.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_nombAreaTrab.Name = "lbl_nombAreaTrab";
+            this.lbl_nombAreaTrab.Size = new System.Drawing.Size(169, 16);
+            this.lbl_nombAreaTrab.TabIndex = 2;
+            this.tagsExtender1.SetTag2(this.lbl_nombAreaTrab, null);
+            this.tagsExtender1.SetTag3(this.lbl_nombAreaTrab, null);
+            this.tagsExtender1.SetTagFontName(this.lbl_nombAreaTrab, fontsD.Lato);
+            this.tagsExtender1.SetTagFontSize(this.lbl_nombAreaTrab, 10F);
+            this.tagsExtender1.SetTagFontStyle(this.lbl_nombAreaTrab, System.Drawing.FontStyle.Bold);
+            this.lbl_nombAreaTrab.Text = "Nombre del Área de Trabajo";
+            // 
+            // pantallaAgregarAreaTrabajo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.txt_AgregarAreaTrabajo);
+            this.Controls.Add(this.roundedPanel1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pnl_nombAreaTrab);
+            this.Controls.Add(this.rpTopMain);
+            this.Name = "pantallaAgregarAreaTrabajo";
+            this.Size = new System.Drawing.Size(611, 470);
+            this.tagsExtender1.SetTag2(this, null);
+            this.tagsExtender1.SetTag3(this, null);
+            this.tagsExtender1.SetTagFontName(this, fontsD.Ninguno);
+            this.tagsExtender1.SetTagFontSize(this, 10F);
+            this.tagsExtender1.SetTagFontStyle(this, System.Drawing.FontStyle.Regular);
+            this.roundedPanel1.ResumeLayout(false);
+            this.pnl_nombAreaTrab.ResumeLayout(false);
+            this.pnl_nombAreaTrab.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private RoundedPanel rpTopMain;
+        private System.Windows.Forms.TextBox txt_AgregarAreaTrabajo;
+        private RoundedPanel roundedPanel1;
+        private System.Windows.Forms.Button btn_guardar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel pnl_nombAreaTrab;
+        private RoundedPanel rnd_color_1;
+        private System.Windows.Forms.TextBox txt_nombAreaTrab;
+        private System.Windows.Forms.Label lbl_nombAreaTrab;
+        private TagsExtender tagsExtender1;
+    }
+}

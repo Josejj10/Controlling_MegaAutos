715ac3e60f394d36900d7695fcff37cded1d5cdd
Jose Shimabukuro <seizenshima@yahoo.es>
2020-05-27 19:51:43 -0500

    Agregado la funcion de editar cliente

--- ProyectosNET/LP2MegaAutos/pantallaEditarCliente.cs
+++ ProyectosNET/LP2MegaAutos/pantallaEditarCliente.cs
@@ -12,9 +12,27 @@ namespace LP2MegaAutos
 {
     public partial class pantallaEditarCliente : UserControl
     {
+        // Singleton instance para poder crearlo en la pantalla menu
+        #region instancia
+        private static pantallaEditarCliente _instancia;
+        public static pantallaEditarCliente Instancia
+        {
+            get
+            {
+                if (_instancia == null)
+                    _instancia = new pantallaEditarCliente();
+                return _instancia;
+            }
+        }
+        #endregion instancia
         public pantallaEditarCliente()
         {
             InitializeComponent();
+        }
+
+        private void btn_guardar_Click(object sender, EventArgs e)
+        {
+            this.SendToBack();
         }
     }
 }

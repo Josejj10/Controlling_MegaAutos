dcb9e33a079acd7c9440da7b18cff9999bcfc446
Rodrigo Dulanto Chicana <rodrigo.dulanto@pucp.edu.pe>
2020-06-24 02:33:16 -0500

    Se actualizaron clases de Sede

--- ProyectosJAVA/LP2MegaAutosModel/src/pe/com/megaautos/model/Sede.java
+++ ProyectosJAVA/LP2MegaAutosModel/src/pe/com/megaautos/model/Sede.java
@@ -3,6 +3,8 @@ package pe.com.megaautos.model;
 public class Sede implements IConsultable{
     private int id;
     private String distrito;
+    private String nombre;
+    private String direccion;
     
     public Sede(){
     }
@@ -25,6 +27,22 @@ public class Sede implements IConsultable{
 
     public void setDistrito(String distrito) {
         this.distrito = distrito;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
     }
     
     public String consultarDatos(){
